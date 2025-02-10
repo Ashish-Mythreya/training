1d4dd7d13efa24541129cd6a9665cc818f0a7b64
Ashish-Mythreya <167844114+Ashish-Mythreya@users.noreply.github.com>
2025-02-10 10:33:58 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
-import React from 'react';
-import './App.css';
-import Welcome from './Welcome';
+import React, { useState } from "react";
+import "./App.css"; // Include your CSS file
 
-// const Welcome=()=><h1 className='message'>Hello Charan</h1>
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const [count, setCount] = useState(0);
 
-// function App(){
-//   return(
-//     <div className='App'>
-//         <Welcome/>  
-//     </div>
-//   )
-// }
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle("dark", !darkMode); // Add this line to toggle the dark mode class on the body element
+  };
 
-const App=()=>{
-  return(
-    <div style={{textAlign:'center',color:'blue'}}>
-      <Welcome greeting="Good morning" name="Charan"/>
-      <Welcome greeting="Hii" name="Murali"/>
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <header>
+        <h1>Light/Dark Toggle & Counter</h1>
+        <input
+          type="checkbox"
+          className="checkbox"
+          id="checkbox"
+          onChange={toggleDarkMode}
+        />
+        <label htmlFor="checkbox" className="checkbox-label">
+          <i className="fas fa-moon"></i>
+          <i className="fas fa-sun"></i>
+          <span className="ball"></span>
+        </label>
+      </header>
+      <main>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </main>
     </div>
-  )
-}
-export default App;
+  );
+};
+
+export default App;
